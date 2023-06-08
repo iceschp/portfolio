9f9a122a6fd8778a92418df20bee47c509d3ea0b
iceschp <warunya.ks@gmail.com>
2023-06-08 17:03:25 +0700

    fix next config

--- app/navbar.js
+++ app/navbar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
     return (
         <div className="navBar">
             <div className="nameLogoBox">
-                <Link href="/app" className="text-xl sm:text-3xl font-bold">Warunya S.</Link>
+                <Link href="/" className="text-xl sm:text-3xl font-bold">Warunya S.</Link>
             </div>
                 <Link href="/info" className="text-base sm:text-xl hover:underline">Info</Link>
         </div>
